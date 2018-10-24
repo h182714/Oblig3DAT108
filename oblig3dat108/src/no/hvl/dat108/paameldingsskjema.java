38632c1ab8c2f91c3f5be9162dfde0b4b37b4584
orjan <orjanen@hotmail.no>
2018-10-24 18:08:13 +0200

    validatorklasse

--- oblig3dat108/src/no/hvl/dat108/paameldingsskjema.java
+++ oblig3dat108/src/no/hvl/dat108/paameldingsskjema.java
@@ -1,14 +1,44 @@
 package no.hvl.dat108;
 
+import javax.servlet.http.HttpServletRequest;
+
 public class paameldingsskjema {
-	
+
 	private String fornavn;
 	private String etternavn;
 	private String mobil;
 	private String passord;
-	private String passordRelatert;
+	private String passordRepeater;
+
+	public paameldingsskjema(HttpServletRequest request) {
+
+		this.fornavn = request.getParameter("fornavn");
+		this.etternavn = request.getParameter("etternavn");;
+		this.mobil = request.getParameter("mobil");;
+		this.passord = request.getParameter("passord");;
+		this.passordRepeater = request.getParameter("passordRepetert");;
+	}
+
+	public String getFornavn() {
+		return fornavn;
+	}
+
+	public String getEtternavn() {
+		return etternavn;
+	}
+
+	public String getMobil() {
+		return mobil;
+	}
+
+	public String getPassord() {
+		return passord;
+	}
+
+	public String getPassordRepeater() {
+		return passordRepeater;
+	}
 	
-		
 	
 
 }
